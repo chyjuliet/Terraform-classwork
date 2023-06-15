b1b27594c8979f52632de0fa3ad055861df246be
chyjuliet <jezeunara@gmail.com>
2023-06-15 17:28:38 -0400

    security-group.tf

---
+++ security-group.tf
@@ -0,0 +1,33 @@
+# SECURITY GROUP
+resource "aws_security_group" "allow_food-1" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.food-1-vpc.id
+
+  ingress {
+    description      = "ssh from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "http from www"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "food-1 sg"
+  }
+}
